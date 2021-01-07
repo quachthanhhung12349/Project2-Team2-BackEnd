a7b05bf4ebdcae9c163c40b65cead19015ad1a5b
haochengca <blueskins@gmail.com>
2021-01-07 16:14:19 -0500

    configure swagger

--- src/main/java/com/revature/cotroller/Haocheng.java
+++ src/main/java/com/revature/cotroller/Haocheng.java
@@ -18,11 +18,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 
 @RestController
+@EnableSwagger2
 public class Haocheng {
 
     @Autowired
